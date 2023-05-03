cbadfc2ecca603f8b1e2fe7ce8becc25f3a56cb6
lailanurfardah <lailanurfardah1707@gmail.com>
2023-05-03 20:32:37 +0700

    add js script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 let height = document.getElementById("height");
 let weight = document.getElementById("weight");
 let age = document.getElementById("age");
+let male = document.getElementById("m");
+let female = document.getElementById("f");
